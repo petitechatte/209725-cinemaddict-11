886cda0f2ded3b77ca55a9a6c2f847845472409d
petitechatte <natalyavaskot@gmail.com>
2020-04-03 15:35:35 +0300

    Добавлена отрисовка блока пользователя в шапке

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 "use strict";
 
+const siteHeader = document.querySelector(`.header`);
+
+// функция отрисовки шаблона
+
 const renderTemplate = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(template, place);
+  container.insertAdjacentHTML(place, template);
 };
+
+// шаблоны компонентов
+
+const createUserProfile = () => {
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`
+  );
+};
+
+// отрисовка компонентов
+
+renderTemplate(siteHeader, createUserProfile());
