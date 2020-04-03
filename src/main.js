2d5600a7a75bc07b14c31278616be7133e8c82f0
petitechatte <natalyavaskot@gmail.com>
2020-04-03 15:46:13 +0300

    Добавлена отрисовка навигации по сайту

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const siteHeader = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
 
 // функция отрисовки шаблона
 
@@ -19,6 +20,21 @@ const createUserProfile = () => {
   );
 };
 
+const createSiteNavigation = () => {
+  return (
+    `<nav class="main-navigation">
+      <div class="main-navigation__items">
+        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      </div>
+      <a href="#stats" class="main-navigation__additional">Stats</a>
+    </nav>`
+  );
+};
+
 // отрисовка компонентов
 
 renderTemplate(siteHeader, createUserProfile());
+renderTemplate(siteMainElement, createSiteNavigation());
