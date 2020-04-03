f3b0268137e8dc6c55a45cf2fbbbab8bd5675a31
petitechatte <natalyavaskot@gmail.com>
2020-04-03 17:11:46 +0300

    Добавлена отрисовка основного раздела с карточками фильмов

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const CARDS_MAIN_NUMBER = 5; // количество карточек в основном разделе
+
 const siteHeader = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 
@@ -34,7 +36,72 @@ const createSiteNavigation = () => {
   );
 };
 
-// отрисовка компонентов
+const createSortingControls = () => {
+  return (
+    `<ul class="sort">
+      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" class="sort__button">Sort by date</a></li>
+      <li><a href="#" class="sort__button">Sort by rating</a></li>
+    </ul>`
+  );
+};
+
+const createFilmsGeneralContainer = () => {
+  return (
+    `<section class="films">
+      <section class="films-list">
+        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      </section>
+    </section>`
+  );
+};
+
+const createFilmsListContainer = () => {
+  return (
+    `<div class="films-list__container"></div>`
+  );
+};
+
+const createFilmCard = () => {
+  return (
+    `<article class="film-card">
+      <h3 class="film-card__title">The Dance of Life</h3>
+      <p class="film-card__rating">8.3</p>
+      <p class="film-card__info">
+        <span class="film-card__year">1929</span>
+        <span class="film-card__duration">1h 55m</span>
+        <span class="film-card__genre">Musical</span>
+      </p>
+      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+      <a class="film-card__comments">5 comments</a>
+      <form class="film-card__controls">
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+      </form>
+    </article>`
+  );
+};
+
+// отрисовка основных разделов сайта
 
 renderTemplate(siteHeader, createUserProfile());
 renderTemplate(siteMainElement, createSiteNavigation());
+renderTemplate(siteMainElement, createSortingControls());
+renderTemplate(siteMainElement, createFilmsGeneralContainer());
+
+// отрисовка подразделов для контента
+
+const filmsGeneralContainer = siteMainElement.querySelector(`.films`);
+const filmsMainSection = filmsGeneralContainer.querySelector(`.films-list`);
+
+renderTemplate(filmsMainSection, createFilmsListContainer());
+
+// отрисовка карточек фильмов
+
+const filmsListContainer = filmsMainSection.querySelector(`.films-list__container`);
+
+for (let i = 0; i < CARDS_MAIN_NUMBER; i++) {
+  renderTemplate(filmsListContainer, createFilmCard());
+}
